18269421e44b92ca5ebba38531fdb31af8f35a13
Mark Keck <mark.keck.jr@gmail.com>
2026-05-02 15:41:42 -0400

    (air quotes) Working version of Lightning training, but not yet tested for generation.

--- src/keck_dl/llm/training/trainers.py
+++ src/keck_dl/llm/training/trainers.py
@@ -9,14 +9,21 @@ class NextTokenPretraining(L.LightningModule):
 
     def training_step(self, batch):
         x, targets = batch
+        x = torch.stack(x)
+        targets = torch.stack(targets)
 
         # the model generates logits in the output vocab space for these
         # models, so we have to pop a cross-entropy loss on the end
         loss_class = torch.nn.CrossEntropyLoss()
+        in_shape = x.shape
+        reshape_sz = in_shape[0] * in_shape[1]
         logits = self.model(x)
-        loss = loss_class(logits, target)
+        logits = torch.reshape(logits, (reshape_sz, logits.shape[-1]))
+        loss = loss_class(logits, targets.flatten())
+        self.log("train_loss", loss, prog_bar=True, on_step=True)
         return loss
 
     def configure_optimizers(self):
-        opt = torch.nn.Adam(self.parameters, lr=1e-3))
+        opt = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         return opt
+
